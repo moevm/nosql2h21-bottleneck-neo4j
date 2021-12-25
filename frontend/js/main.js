81122f07d17f4def44d36311beab33d5fa596727
MatrosovDenis <qwerty_19200@mail.ru>
2021-12-25 18:19:33 +0300

    Точки выбранного полигона

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,20 +8,22 @@ ymaps.ready(['ext.paintOnMap']).then(function () {
     var paintProcess;
 
     // Опции многоугольника или линии.
-    var styles = {strokeColor: '#00ff00', strokeOpacity: 0.5, strokeWidth: 3, fillColor: '#00ff00', fillOpacity: 0.2};
+    var styles = {strokeColor: '#0000ff', strokeOpacity: 0.5, strokeWidth: 3, fillColor: '#0000ff', fillOpacity: 0.2};
 
     var currentIndex = 0;
+
+    var traffic_jams = new ymaps.control.Button({data: {content: 'Пробки'}, options: {maxWidth: 150}});
+    map.controls.add(traffic_jams);
+
+    var button = new ymaps.control.Button({data: {content: 'Узкие места'}, options: {maxWidth: 150}});
+    map.controls.add(button);
+
 
     // Подпишемся на событие нажатия кнопки мыши.
     map.events.add('mousedown', function (e) {
         // Если кнопка мыши была нажата с зажатой клавишей "alt", то начинаем рисование контура.
         if (e.get('altKey')) {
-            if (currentIndex == styles.length - 1) {
-                currentIndex = 0;
-            } else {
-                currentIndex += 1;
-            }
-            paintProcess = ymaps.ext.paintOnMap(map, e, {style: styles[currentIndex]});
+            paintProcess = ymaps.ext.paintOnMap(map, e, {style: styles});
         }
     });
 
@@ -32,17 +34,18 @@ ymaps.ready(['ext.paintOnMap']).then(function () {
             // Получаем координаты отрисованного контура.
             var coordinates = paintProcess.finishPaintingAt(e);
             paintProcess = null;
-            // В зависимости от состояния кнопки добавляем на карту многоугольник или линию с полученными координатами.
             var geoObject = new ymaps.Polygon([coordinates], {}, styles[currentIndex]);
-
+            var COORD = [];
+            for(var i = 0; i < coordinates.length; i++){
+                COORD[i] = [
+                    coordinates[i][0],
+                    coordinates[i][1]
+                ]
+            }
+            var JSON_COORDS = JSON.stringify(COORD);
+            console.log(JSON_COORDS);
             map.geoObjects.add(geoObject);
         }
     });
-
-    var button = new ymaps.control.Button({data: {content: 'Пробки'}, options: {maxWidth: 150}});
-    map.controls.add(button);
-
-    var button = new ymaps.control.Button({data: {content: 'Узкие места'}, options: {maxWidth: 150}});
-    map.controls.add(button);
 
 }).catch(console.error);
