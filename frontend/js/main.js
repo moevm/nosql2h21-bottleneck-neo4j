7f46881063f573e8f519c7b6778f216fde575e7d
MatrosovDenis <qwerty_19200@mail.ru>
2021-12-25 21:39:14 +0300

    Выделение узких мест разными цветами

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,7 +2,7 @@ ymaps.ready(['ext.paintOnMap']).then(function () {
     var map = new ymaps.Map('map', {
         center: [55.75, 37.62],
         zoom: 14,
-        controls: ["zoomControl", 'trafficControl']
+        controls: ["zoomControl", 'trafficControl', 'geolocationControl', "searchControl"]
     });
 
     var paintProcess;
@@ -10,9 +10,12 @@ ymaps.ready(['ext.paintOnMap']).then(function () {
     // Опции многоугольника или линии.
     var styles = {strokeColor: '#0000ff', strokeOpacity: 0.5, strokeWidth: 3, fillColor: '#0000ff', fillOpacity: 0.2};
 
+    var button = new ymaps.control.Button({data: {content: 'Режим выделеия'}, options: {maxWidth: 150}});
+    map.controls.add(button);
+
     map.events.add('mousedown', function (e) {
         // Если кнопка мыши была нажата с зажатой клавишей "alt", то начинаем рисование контура.
-        if (e.get('altKey')) {
+        if (e.get('altKey') || button.isSelected()) {
             paintProcess = ymaps.ext.paintOnMap(map, e, {style: styles});
         }
     });
@@ -37,8 +40,33 @@ ymaps.ready(['ext.paintOnMap']).then(function () {
             map.geoObjects.add(geoObject);
             GetBottlenecks(JSON_COORDS, function(Response){
                 console.log(Response.responseText);
+                var json = JSON.parse(Response.responseText);
+                for(var i = 0; i < json.length; i++){
+                    console.log(JSON.stringify(json[i]["points"]));
+                    var polyline = new ymaps.Polyline(json[i]["points"], {}, {
+                        strokeWidth: 5,
+                        strokeColor: perc2color(100 - (json[i]["load"] * 100)),
+                        opacity: 0.5
+                    });
+                    map.geoObjects.add(polyline);
+                }
             })
         }
     });
 
 }).catch(console.error);
+
+
+function perc2color(perc) {
+    var r, g, b = 0;
+    if(perc < 50) {
+        r = 255;
+        g = Math.round(5.1 * perc);
+    }
+    else {
+        g = 255;
+        r = Math.round(510 - 5.10 * perc);
+    }
+    var h = r * 0x10000 + g * 0x100 + b * 0x1;
+    return '#' + ('000000' + h.toString(16)).slice(-6);
+}
