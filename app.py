3f00e61a76bbe275430b264812110302151d50a4
Wintari <dimonx2000.ru@mail.ru>
2021-12-25 18:42:54 +0300

    Add basic flask response

---
+++ app.py
@@ -0,0 +1,29 @@
+import overpass
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+api = overpass.API()
+
+mapRequest = '(\
+                node(poly: "%(poly)s");\
+                relation(poly: "%(poly)s");\
+                <;\
+                >;\
+               );'
+
+@app.route('/polygon', methods=['POST'])
+def getByPolygon():
+    try:
+        data = json.loads(request.get_data())
+        points = []
+        if(data):
+            points = ["%f %f" % tuple(point) for point in data]
+        
+        result = api.get(mapRequest % {"poly": (" ").join(points)})
+        return json.dumps(result, ensure_ascii=False), 200
+    except Exception:
+        return None, 400
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
